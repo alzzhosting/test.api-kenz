ee3863606c4c3c428fb0a74fe0470b9ce48fe60b
KenzDev <alzzoffchosting@gmail.com>
2025-01-29 19:24:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({ 
         status: "success", 
         message: "API KenzDev berhasil diakses!",
